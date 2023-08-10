0b2330904b985e960cc618fdf13271cbad117d6e
Gourav Chaki <104303510+GouravChaki@users.noreply.github.com>
2023-08-10 16:59:32 +0530

    updateCharacters added

--- Router/character_routes.js
+++ Router/character_routes.js
@@ -3,11 +3,14 @@ const router = express.Router();
 const create_character=require('../Modules/characters/create_character');
 const read_character = require('../Modules/characters/read_character');
 const fetch_all_characters = require('../Modules/characters/fetch_all_characters');
+const update_character = require('../Modules/characters/update_character');
 
 router.post('/createCharacter', create_character);//createcharacter route will create/enter character_details
 
 router.post('/fetchCharacter', read_character);//fetchCharacter route will fetch character_details
 
 router.post('/fetchAllCharacters', fetch_all_characters);//fetchAllCharacters route will fetch all the character_details from the database
+
+router.post('/updateCharacters', update_character);//updateCharacters route will update all the character_details from the database
 
 module.exports = router;
